5576232a2a786f422a6bad0825f26c3dd582a8ee
hugo <hdamitzian@gmail.com>
2024-07-05 14:24:40 +0200

    init raycasting typecript

---
+++ index.ts
@@ -0,0 +1,14 @@
+(() => {
+    //recuperer l'element id canvqs
+    const canva = document.getElementById('game') as (HTMLCanvasElement | null);
+    if (canva === null) {
+        throw new Error('Impossible de trouver le canvas');
+    }
+    const ctx = canva.getContext('2d');
+    if (ctx === null) {
+        throw new Error('Impossible de récupérer le contexte 2D');
+    }
+    ctx.fillStyle = "rgb(200,0,0)"; // définit la couleur de remplissage du rectangle
+    ctx.fillRect(0, 0, canva.width, canva.height); // dessine un rectangle plein
+    console.log(ctx);  
+})();
